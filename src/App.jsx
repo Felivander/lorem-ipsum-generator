1f5562bc50052398ad7877fff4773ce31b25c04b
felivander <feli.vander@gmail.com>
2023-10-25 17:23:46 -0300

    need to add github link

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import textData from "./data.js";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText(textData.slice(0, count));
+  };
+
+  return (
+    <>
+      <section className="section-center">
+        <h3>Tired of boring lorem ipsum?</h3>
+        <form className="lorem-form" onSubmit={handleSubmit}>
+          <label htmlFor="amount">paragraphs:</label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={count}
+            max="8"
+            min="1"
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <button type="submit" className="btn">
+            Generate
+          </button>
+        </form>
+        <article className="lorem-text">
+          {text.map((text, index) => {
+            return <p key={index}>{text}</p>;
+          })}
+        </article>
+      </section>
+      <footer>
+        <h4>
+          Made by{" "}
+          <span>
+            <a href="https://github.com/Felivander">@felivander</a>
+          </span>
+        </h4>
+        <h4>
+          Code on{" "}
+          <a id="github" href="">
+            Github
+          </a>
+        </h4>
+      </footer>
+    </>
+  );
+}
+
+export default App;
